32e0810bdb1fa687bf4b223c9a72884a25b9b1da
Jordy <jordycorporan95@gmail.com>
2023-12-29 16:49:31 -0500

    test ice cream with logger

--- reduxtoolkit/index.js
+++ reduxtoolkit/index.js
@@ -1,5 +1,6 @@
 const store = require("./app/store");
 const cakeActions = require("./app/features/cake/cakeSlice").cakeActions;
+const icecreamActions = require('./app/features/icecream/icecreamSlice').icecreamActions
 console.log("Initial State", store.getState());
 const unsubscribe = store.subscribe(() => {
   console.log("Updated State", store.getState());
@@ -10,5 +11,13 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.reStocked(3));
+
+// test icecreams actions
+
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(5))
+
 
 unsubscribe();
